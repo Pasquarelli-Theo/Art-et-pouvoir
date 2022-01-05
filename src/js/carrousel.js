ad81d8ac893e6d25f2a5c08c644d6ff0a18b02cb
Pasquarelli-Theo <theo.pasquarelli@edu.univ-fcomte.fr>
2022-01-05 15:41:08 +0100

    commit carrousel

---
+++ src/js/carrousel.js
@@ -0,0 +1,37 @@
+function carrousel() {
+    var images4 = document.querySelectorAll('.image4');
+    let d = 2000;
+    let delta = 1000;
+
+    images4.forEach(function(img,indice) {
+        img.style.zIndex = images4.length - indice;
+    });
+
+    let anim4 = anime.timeline({
+        loop: true,
+        delay: 0
+    });
+
+    anim4.add({
+        targets: images4,
+        translateX: [{value: '250', duration: d},
+                     {value: '-250', duration: 0, delay: function(img,ind) {
+                         if (ind == 0) return (3*delta)+(2*d);
+                         if (ind == 1) return (2*delta)+d;
+                         if (ind == 2) return delta;
+                         return 0;
+                     }},
+                     {value: '0', duration: function(img,ind) {
+                         if (ind == 3) return 0;
+                         return d;
+                     }}],
+        easing: 'linear',
+        delay: function(img,ind) {
+            if (ind == 0) return delta;
+            if (ind == 1) return (2*delta)+d;
+            if (ind == 2) return (3*delta)+(2*d);
+            return (4*delta)+(3*d);
+        }
+    });
+	
+}
